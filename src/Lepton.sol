eea652ed8d9071266b8ebd66b635ea2ccf00c4e9
Paul Reinholdtsen <paul@bitsy.services>
2026-03-09 12:11:38 -0500

    Use full variable names

--- src/Lepton.sol
+++ src/Lepton.sol
@@ -15,40 +15,40 @@ contract Lepton is ICoinage, ERC20 {
     constructor() ERC20("Lepton Factory", "PROTOTYPE") {}
 
     /// @inheritdoc ICoinage
-    function made(string calldata n, string calldata s, uint256 t)
+    function made(string calldata name, string calldata symbol, uint256 supply)
         public
         view
-        returns (bool yes, address home, bytes32 salt)
+        returns (bool deployed, address home, bytes32 salt)
     {
-        if (bytes(n).length == 0) revert Nameless();
-        if (bytes(s).length == 0) revert Symbolless();
-        if (t == 0) revert Nothing();
-        salt = keccak256(abi.encode(n, s, t));
+        if (bytes(name).length == 0) revert Nameless();
+        if (bytes(symbol).length == 0) revert Symbolless();
+        if (supply == 0) revert Nothing();
+        salt = keccak256(abi.encode(name, symbol, supply));
         home = Clones.predictDeterministicAddress(address(PROTOTYPE), salt, address(PROTOTYPE));
-        yes = home.code.length > 0;
+        deployed = home.code.length > 0;
     }
 
     /// @inheritdoc ICoinage
-    function make(string calldata n, string calldata s, uint256 t) external returns (ICoinage lepton) {
-        (bool deployed, address home, bytes32 salt) = made(n, s, t);
-        lepton = ICoinage(home);
+    function make(string calldata name, string calldata symbol, uint256 supply) external returns (ICoinage token) {
+        (bool deployed, address home, bytes32 salt) = made(name, symbol, supply);
+        token = ICoinage(home);
         if (deployed) {
             // return the deployed contract address.
         } else {
             home = Clones.cloneDeterministic(address(PROTOTYPE), salt, 0);
-            Lepton(home).zzz_(msg.sender, n, s, t);
+            Lepton(home).zzz_(msg.sender, name, symbol, supply);
         }
     }
 
     /// @notice Initialiser called by the prototype on a freshly deployed clone.
     /// @dev Reverts with {Unauthorized} otherwise.
-    function zzz_(address maker, string calldata n, string calldata s, uint256 t) public {
+    function zzz_(address maker, string calldata name, string calldata symbol, uint256 supply) public {
         if (msg.sender != address(PROTOTYPE)) {
             revert Unauthorized();
         }
-        _name = n;
-        _symbol = s;
-        _mint(maker, t);
-        emit Make(maker, ICoinage(address(this)), n, s, t);
+        _name = name;
+        _symbol = symbol;
+        _mint(maker, supply);
+        emit Make(maker, ICoinage(address(this)), name, symbol, supply);
     }
 }
