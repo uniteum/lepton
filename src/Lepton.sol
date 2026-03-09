f39b427984e479046782a2ab05b2438fc7545ef1
Paul Reinholdtsen <paul@bitsy.services>
2026-03-09 12:19:51 -0500

    Simplify code.

--- src/Lepton.sol
+++ src/Lepton.sol
@@ -5,12 +5,12 @@ import {ICoinage} from "ierc20/ICoinage.sol";
 import {ERC20} from "erc20/ERC20.sol";
 import {Clones} from "clones/Clones.sol";
 
-/// @notice Minimalist fixed-supply ERC-20 maker. A single call to {make}
-///         deploys a new ERC-20 clone and mints the entire supply to the caller.
+/// @notice Minimalist fixed-supply ERC-20 maker.
+///         Calling {make} deploys a new clone and mints the entire supply to the caller.
 /// @author Paul Reinholdtsen (reinholdtsen.eth)
 contract Lepton is ICoinage, ERC20 {
     /// @notice The prototype instance used as the EIP-1167 implementation.
-    Lepton public immutable PROTOTYPE = this;
+    address public immutable PROTOTYPE = address(this);
 
     constructor() ERC20("Lepton Factory", "PROTOTYPE") {}
 
@@ -24,7 +24,7 @@ contract Lepton is ICoinage, ERC20 {
         if (bytes(symbol).length == 0) revert Symbolless();
         if (supply == 0) revert Nothing();
         salt = keccak256(abi.encode(name, symbol, supply));
-        home = Clones.predictDeterministicAddress(address(PROTOTYPE), salt, address(PROTOTYPE));
+        home = Clones.predictDeterministicAddress(PROTOTYPE, salt, PROTOTYPE);
         deployed = home.code.length > 0;
     }
 
@@ -35,7 +35,7 @@ contract Lepton is ICoinage, ERC20 {
         if (deployed) {
             // return the deployed contract address.
         } else {
-            home = Clones.cloneDeterministic(address(PROTOTYPE), salt, 0);
+            home = Clones.cloneDeterministic(PROTOTYPE, salt, 0);
             Lepton(home).zzz_(msg.sender, name, symbol, supply);
         }
     }
@@ -43,7 +43,7 @@ contract Lepton is ICoinage, ERC20 {
     /// @notice Initialiser called by the prototype on a freshly deployed clone.
     /// @dev Reverts with {Unauthorized} otherwise.
     function zzz_(address maker, string calldata name, string calldata symbol, uint256 supply) public {
-        if (msg.sender != address(PROTOTYPE)) {
+        if (msg.sender != PROTOTYPE) {
             revert Unauthorized();
         }
         _name = name;
