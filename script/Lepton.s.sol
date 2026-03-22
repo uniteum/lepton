b36cc808412a4ada4404be0d713a297ad5e118ce
Paul Reinholdtsen <paul@bitsy.services>
2026-03-22 18:58:03 -0500

    Use crucible framework

--- script/Lepton.s.sol
+++ script/Lepton.s.sol
@@ -1,18 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import {Script, console2} from "forge-std/Script.sol";
 import {Lepton} from "../src/Lepton.sol";
+import {ProtoScript} from "crucible/script/Proto.s.sol";
 
 /// @notice Deploy the Lepton contract.
 /// @dev Usage: forge script script/Lepton.s.sol -f $chain --private-key $tx_key --broadcast --verify --delay 10 --retries 10
-contract LeptonProto is Script {
-    function run() public {
-        vm.startBroadcast();
+contract LeptonProto is ProtoScript {
+    function name() internal pure override returns (string memory) {
+        return "LeptonProto";
+    }
 
-        Lepton proto = new Lepton{salt: 0x0}();
-        console2.log("Deployed Lepton proto at:", address(proto));
-
-        vm.stopBroadcast();
+    function creationCode() internal pure override returns (bytes memory) {
+        return type(Lepton).creationCode;
     }
 }
