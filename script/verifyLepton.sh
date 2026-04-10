b905c054efafcd1e685a0c8ff2745e0ad10998d0
Paul Reinholdtsen <paul@bitsy.services>
2026-04-10 10:28:53 -0500

    verify Lepton

--- script/verifyLepton.sh
+++ script/verifyLepton.sh
@@ -1,2 +1,2 @@
 contract=$(jq -r '.transactions[0].contractAddress' broadcast/Lepton.s.sol/$chain/dry-run/run-latest.json)
-forge verify-contract $contract Lepton --chain $chain --verifier etherscan --show-standard-json-input > io/$chain/Lepton.json
+forge verify-contract $contract Lepton --chain $chain --verifier etherscan --show-standard-json-input > io/Lepton.json
