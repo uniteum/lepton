0bf34f4f1f6ec4758a50d6ee2f06e2e4b0841e2d
Paul Reinholdtsen <paul@bitsy.services>
2026-05-02 12:43:44 -0500

    lowercase proto and version

--- src/Lepton.sol
+++ src/Lepton.sol
@@ -12,10 +12,10 @@ import {Clones} from "clones/Clones.sol";
  * @author Paul Reinholdtsen (reinholdtsen.eth)
  */
 contract Lepton is ICoinage, ERC20 {
-    string public constant VERSION = "0.2.0";
+    string public constant version = "1.0.0";
 
     /// @notice The prototype instance used as the EIP-1167 implementation.
-    address public immutable PROTO = address(this);
+    address public immutable proto = address(this);
 
     uint8 internal _decimals;
 
@@ -41,7 +41,7 @@ contract Lepton is ICoinage, ERC20 {
         if (bytes(symbol).length == 0) revert Symbolless();
         if (supply == 0) revert Nothing();
         salt = keccak256(abi.encode(maker, name, symbol, decimals_, supply, variant));
-        home = Clones.predictDeterministicAddress(PROTO, salt, PROTO);
+        home = Clones.predictDeterministicAddress(proto, salt, proto);
         deployed = home.code.length > 0;
     }
 
@@ -55,7 +55,7 @@ contract Lepton is ICoinage, ERC20 {
         if (deployed) {
             // return the deployed contract address.
         } else {
-            home = Clones.cloneDeterministic(PROTO, salt, 0);
+            home = Clones.cloneDeterministic(proto, salt, 0);
             Lepton(home).zzInit(msg.sender, name, symbol, decimals_, supply);
             emit Made(msg.sender, token, name, symbol, decimals_, supply);
         }
@@ -68,7 +68,7 @@ contract Lepton is ICoinage, ERC20 {
     function zzInit(address maker, string calldata name, string calldata symbol, uint8 decimals_, uint256 supply)
         public
     {
-        if (msg.sender != PROTO) revert Unauthorized();
+        if (msg.sender != proto) revert Unauthorized();
         _name = name;
         _symbol = symbol;
         _decimals = decimals_;
